84c57e78cf68a0794f8c2d6380c175c3612daedd
jakepro657 <jakepro657@gmail.com>
2025-02-26 16:51:27 +0900

    fix: blob and url

--- src/app/api/sticker/route.ts
+++ src/app/api/sticker/route.ts
@@ -59,82 +59,82 @@ export async function POST(request: NextRequest) {
   //   return NextResponse.json({ error: "No image provided" }, { status: 400 });
   // }
 
-  // const gptResponse = await openai.chat.completions.create({
-  //   model: "gpt-4o-2024-05-13",
-  //   messages: [
-  //     {
-  //       role: "system",
-  //       content: `
+  const gptResponse = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "system",
+        content: `
 
-  //       <context>
-  //         output format is string only with just one word.
+        <context>
+          output format is string only with just one word.
 
-  //         example:
-  //         - Man
-  //         - Woman
-  //       </context>
+          example:
+          - Man
+          - Woman
+        </context>
 
-  //       <instruction>
-  //         Distinguish man or woman in the image.
-  //       </instruction>
-  //       `,
-  //     },
-  //     {
-  //       role: "user",
-  //       content: [
-  //         {
-  //           type: "image_url",
-  //           image_url: {
-  //             url: originalUrl
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // });
+        <instruction>
+          Distinguish man or woman in the image.
+        </instruction>
+        `,
+      },
+      {
+        role: "user",
+        content: [
+          {
+            type: "image_url",
+            image_url: {
+              url: originalUrl
+            },
+          },
+        ],
+      },
+    ],
+  });
 
-  // const gender = gptResponse.choices[0].message.content as string;
+  const gender = gptResponse.choices[0].message.content as string;
 
-  // const gptReponseForDetailed = await openai.chat.completions.create({
-  //   model: "gpt-4o-2024-05-13",
-  //   messages: [
-  //     {
-  //       role: "system",
-  //       content: `
+  const gptReponseForDetailed = await openai.chat.completions.create({
+    model: "gpt-4o-2024-05-13",
+    messages: [
+      {
+        role: "system",
+        content: `
 
-  //       <context>
-  //         output format is string only with just one words separated by comma.
+        <context>
+          output format is string only with just one words separated by comma.
 
-  //         example
-  //         - a person, white blouse, long hair, glasses, smiling
-  //       </context>
+          example
+          - a person, white blouse, long hair, glasses, smiling
+        </context>
 
-  //       <instruction>
-  //         Describe the image in detail.
-  //         !important: only 5 features of the person in the image.
-  //       </instruction>
-  //       `,
-  //     },
-  //     {
-  //       role: "user",
-  //       content: [
-  //         {
-  //           type: "image_url",
-  //           image_url: {
-  //             url: originalUrl
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // });
+        <instruction>
+          Describe the image in detail.
+          !important: only 5 features of the person in the image.
+        </instruction>
+        `,
+      },
+      {
+        role: "user",
+        content: [
+          {
+            type: "image_url",
+            image_url: {
+              url: originalUrl
+            },
+          },
+        ],
+      },
+    ],
+  });
 
   // 턱수염이 조금 있고 뒷 배경의 색감이 알록달록 하면 좋겠어.
   let detailedPrompt = null;
 
   if (prompt) {
     detailedPrompt = await openai.chat.completions.create({
-      model: "gpt-4o",
+      model: "gpt-4o-2024-05-13",
       messages: [
         {
           role: "system",
@@ -160,19 +160,20 @@ export async function POST(request: NextRequest) {
     });
   }
 
-  // const description = gptReponseForDetailed.choices[0].message
-  //   .content as string;
+  const description = gptReponseForDetailed.choices[0].message
+    .content as string;
+
+  console.log ("description", description);
+
+  console.log("url", originalUrl);
 
   const output = await replicate.run(process.env.REPLICATE_MODEL as any, {
     input: {
-      num_steps: 50,
-      input_image: originalUrl,
-      // prompt: `(masterpiece), (detailed), frontal face, ID photo, ${gender}, ${description}, ${
-      //   prompt == "" ? "" : detailedPrompt
-      // }`,
-      prompt: `(masterpiece), (detailed), frontal face, ID photo ${
+      prompt: `(masterpiece), (detailed), frontal face, ID photo, ${gender}, ${description}, ${
         prompt == "" ? "" : detailedPrompt
       }`,
+      num_steps: 50,
+      input_image: originalUrl == "" ? blob : originalUrl,
       negative_prompt:
         "face painting, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, low resolution, 3d model, deformed hands, deformed feet, deformed face, deformed body parts, same haircut, eyes without pupils, doubled image, mid aged man, old men, logo in frame, gun, man with more than one penis on body, scared facial expression, drawing, painting, blur focus, blur, photo effects, skinny guy, make-up on male, angry facial expression, same human face in one frame, illustration, anime, cartoon, ugly face, bruises, cartoon, anime, painting, red color saturation, unattractive face, jpeg artifacts, frame, Violence, Gore, Blood, War, Weapons, Death, Destruction, Fire, Explosions, Pollution, Garbage, Graffiti, Vandalism, Rust, Decay, Filth, Disease, Insects, Rodents, Vermin, Darkness, Shadows, Nightmares, Fear, Horror, Sadness, Depression, Pain, Suffering, Anguish, Despair, Loneliness, Isolation, Neglect, Abandonment, Negativity, Hate, Racism, Sexism, Homophobia, Discrimination, Intolerance, Prejudice, Ignorance, Arrogance, Greed, Selfishness, Cruelty, Insanity, Madness, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, deformed, ugly, mutilated, disfigured, text, extra limbs, face cut, head cut, extra fingers, extra arms, poorly drawn face, mutation, bad proportions, cropped head, malformed limbs, mutated hands, fused fingers, long neck, illustration, painting, drawing, art, sketch, disfigured, kitsch, ugly, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ugly, disgusting, poorly drawn, childish, mutilated, , mangled, old, surreal, decoration, particles",
       style_strength_ratio: 35
@@ -198,7 +199,7 @@ export async function POST(request: NextRequest) {
   await prisma?.original.create({
     data: {
       url: originalUrl,
-      gender: "X",
+      gender: gender,
       filename: originalFileName,
       userId: foundUser?.id,
     },
@@ -207,7 +208,7 @@ export async function POST(request: NextRequest) {
   await prisma?.generated.create({
     data: {
       url: url,
-      gender: "X",
+      gender: gender,
       filename: filename,
       userId: foundUser?.id,
     },
